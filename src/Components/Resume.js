36618e300c51b325b85686cf88c396234be2eb0c
SabaAbramishvili <abramishvilisabu@gmail.com>
2023-02-08 19:00:17 +0400

    new Commit

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,14 +1,5 @@
-import { Formik, Form, Field, useFormikContext } from "formik";
-import * as Yup from "yup";
 import { useEffect, useRef, React, useState } from "react";
 import isEqual from "react-fast-compare";
-import { useDebouncedCallback } from "use-debounce";
-import Navbar from "./Navbar";
-import correcticon from "./Images/correcticon.svg";
-import erroricon from "./Images/erroricon.svg";
-import Inputs from "./Inputs";
-import Input2 from "./Input2";
-import FormikPersist from "./FormikPersist";
 
 export default function Resume({ changed }) {
   const [values, setValues] = useState("");
